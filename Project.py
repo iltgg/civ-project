10a46ea5eaf978da88be98faea594505674ea2bf
KCTaka <117336741+KCTaka@users.noreply.github.com>
2022-11-03 01:39:26 -0400

    Update Project.py

--- Project.py
+++ Project.py
@@ -12,9 +12,6 @@ def M(train, bridge, x_t, x):
 def N(train, bridge, x_t, x):
     pass
 
-
-
-
 if __name__ == '__main__':
     train = {"weight": 400, "height": 85, "width": 75, "whl_width": 10, "btw_whl": 176, "edge": 52, "btw_train": 60, "n_seg": 3}
     bridge = {"length": 1250, "weight": 0}
