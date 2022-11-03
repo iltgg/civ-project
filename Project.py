3f8e4cabc834f50434173754be2eb35955eebac1
KCTaka <117336741+KCTaka@users.noreply.github.com>
2022-11-03 01:38:21 -0400

    Add files via upload

---
+++ Project.py
@@ -0,0 +1,21 @@
+def I(bridge):
+    return 40**4/12
+
+def V(train, bridge, x_t, x):
+    f1 = location + train["edge"]
+
+    pass
+
+def M(train, bridge, x_t, x):
+    pass
+
+def N(train, bridge, x_t, x):
+    pass
+
+
+
+
+if __name__ == '__main__':
+    train = {"weight": 400, "height": 85, "width": 75, "whl_width": 10, "btw_whl": 176, "edge": 52, "btw_train": 60, "n_seg": 3}
+    bridge = {"length": 1250, "weight": 0}
+    x_t = 10
